8cc8a477616b974aafe863690bcb3aeba3b451dc
l b <poumtak@yahoo.fr>
2020-03-23 15:48:49 +0100

    bug with description

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :description, presence: true, length: { minimum: 10 }
+  validates :description, presence: true
   has_one_attached :photo
   has_one_attached :video
 end
