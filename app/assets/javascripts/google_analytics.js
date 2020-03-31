c279e9904712bc678a82a1fbdd86c0a9762f9d3b
l b <poumtak@yahoo.fr>
2020-04-01 00:03:46 +0200

    delete branch

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -1,6 +1,6 @@
 document.addEventListener('turbolinks:load', function(event) {
   if (typeof gtag === 'function') {
-    gtag('config', 'UA-XXXXXX', {
+    gtag('config', 'UA-162360559-1', {
       'page_location': event.data.url
     });
   }
